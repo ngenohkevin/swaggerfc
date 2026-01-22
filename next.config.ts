17c12f30f250fd914c1da892d974066460a20569
ngenohkevin <ngenohkevin19@gmail.com>
2026-01-22 14:06:47 +0300

    Add Strapi CMS domain to Next.js image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "swaggerfc-cms.iopulse.cloud",
+      },
     ],
   },
   output: "standalone",
